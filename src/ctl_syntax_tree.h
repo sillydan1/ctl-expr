cccb794df39c6281e0544c57a7bb2e0f37351cb1
Asger Gitz-Johansen <agj@hmkbilcon.dk>
2023-03-14 06:59:20 +0100

    wip

--- src/ctl_syntax_tree.h
+++ src/ctl_syntax_tree.h
@@ -51,7 +51,7 @@ namespace ctl {
     };
 
     // using underlying_syntax_node_t = std::variant<expr::identifier_t, expr::operator_t, expr::root_t, expr::symbol_value_t, location_t, modal_t, quantifier_t>;
-    using underlying_syntax_node_t = std::variant<expr::syntax_tree_t, expr::root_t, location_t, modal_t, quantifier_t>;
+    using underlying_syntax_node_t = std::variant<expr::syntax_tree_t, expr::operator_t, expr::root_t, location_t, modal_t, quantifier_t>;
     struct syntax_node_t : public underlying_syntax_node_t {
         syntax_node_t() : underlying_syntax_node_t{expr::root_t{}} {}
         template<typename T>
